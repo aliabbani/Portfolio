194facbbad3c2829b6004aa5e479ddfb7d4b896a
ali abbani <aliox@ahlam.com>
2021-07-29 16:57:50 +0300

    add local storage

--- data.js
+++ data.js
@@ -2,9 +2,9 @@ const myForm = document.querySelector('form');
 
 myForm.addEventListener('keyup', (e) => {
   e.preventDefault();
-  const nameValue = document.getElementById('name').value;
+  const nameValue = document.getElementById('username').value;
   const emailValue = document.getElementById('email').value;
-  const messageValue = document.getElementById('message').value;
+  const messageValue = document.getElementById('comment').value;
 
   const formData = {
     nameValue,
@@ -14,3 +14,11 @@ myForm.addEventListener('keyup', (e) => {
 
   localStorage.setItem('formData', JSON.stringify(formData));
 });
+
+const retrievedData = JSON.parse(localStorage.getItem('formData'));
+
+window.onload = () => {
+  document.getElementById('username').value = retrievedData.nameValue;
+  document.getElementById('email').value = retrievedData.emailValue;
+  document.getElementById('comment').value = retrievedData.messageValue;
+};
