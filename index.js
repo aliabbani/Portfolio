e0d7071a55efedcb70364bc1e8245e675d9c6385
ali abbani <aliox@ahlam.com>
2021-07-30 15:13:49 +0300

    insert bugs in the form js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const menu = document.querySelector('.menu');
+const menu = document.querySelector('menu');
 const closeItem = document.querySelector('.close-item');
 const portfolioMenu = document.querySelector('.portfolio-menu');
 const aboutMenu = document.querySelector('.about-menu');
-const contactMenu = document.querySelector('.contact-menu');
+const contactMenu = document.querySelector('#contact-menu');
 function hideMenu() {
   const mobileMenu = document.querySelector('.mobile-menu');
   mobileMenu.style.display = 'none';
@@ -14,6 +14,6 @@ function showMenu(e) {
 }
 menu.addEventListener('click', showMenu);
 closeItem.addEventListener('click', hideMenu);
-portfolioMenu.addEventListener('click', hideMenu);
+portfolioMenu.addEventListener('click', hideMenu());
 aboutMenu.addEventListener('click', hideMenu);
 contactMenu.addEventListener('click', hideMenu);
